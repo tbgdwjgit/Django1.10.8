3f265df24045700ee4844b813a84b38647adfa8f
tbgdwjgit <17049185@qq.com>
2018-07-26 09:38:50 +0800

    1

--- webdev/views.py
+++ webdev/views.py
@@ -2,4 +2,5 @@
 from django.http import HttpResponse
 
 def first_page(request):
-    return HttpResponse("<p>世界，你好！！！！</p>")
+    str = '<B>测试</B>'
+    return HttpResponse("<p>世界，你好！！！！</p>" + str)
