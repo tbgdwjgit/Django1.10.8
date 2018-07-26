73c16cf790866615a9030fdf83c0ac62194a7413
tbgdwjgit <17049185@qq.com>
2018-07-26 11:46:52 +0800

    1

--- webdev/views.py
+++ webdev/views.py
@@ -1,6 +1,15 @@
 # -*- coding:utf-8 *-
+'''
 from django.http import HttpResponse
 
 def first_page(request):
     str = '<B>测试</B>'
     return HttpResponse("<p>世界，你好！！！！</p>" + str)
+'''
+
+from django.shortcuts import render
+
+def first_page(request):
+    context          = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
